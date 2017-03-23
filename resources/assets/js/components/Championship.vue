3c0f8bb8d0621f7f514e86d003475987adc223f0
dkuck <dkuck@thatsus.com>
2017-03-23 17:07:30 -0400

    It does a thing!

--- resources/assets/js/components/Championship.vue
+++ resources/assets/js/components/Championship.vue
@@ -1,33 +1,69 @@
 <template>
     <div>    
+        <h3>Choose the Contestants</h3>
+        <ul>
+            <li v-for="(strategy, name) in strategies">
+                <input type="checkbox" v-model="chosen" :value="name" /> {{ name }}
+            </li>
+        </ul>
         <button @click="run">Begin Championship!</button>
 
-        <div v-if="championship">
-
+        <div v-if="report">
+            <div>Matches: {{ report.matches }}
+                <span v-if="report.progress">
+                    {{ Math.floor(report.progress * 100) }}%
+                </span>
+            </div>
+            <table>
+                <tr>
+                    <th>Player 1</th>
+                    <th>Player 2</th>
+                    <th>Winner</th>
+                    <th>Details</th>
+                </tr>
+                <tr v-for="result in report.results">
+                    <td :style="{'font-weight': result.winner === 0 ? 'bold' : ''}">{{ result.players[0].name }}</td>
+                    <td :style="{'font-weight': result.winner === 1 ? 'bold' : ''}">{{ result.players[1].name }}</td>
+                    <td>{{ result.winner === 0 ? 'Player 1' : 'Player 2' }}</td>
+                    <td>...</td>
+                </tr>
+            </table>
         </div>
     </div>
 </template>
 
 <script>
 import Championship from '../Championship.js';
-import SayAnything from '../strategies/SayAnything.js';
 import Whos from '../Whos.js';
+var strategies = [
+    require('../strategies/SayAnything.js'),
+];
 
 export default {
     data() {
+        var strategyMap = {};
+        strategies.map(strategy => strategyMap[strategy.name] = strategy);
         return {
             championship: null,
+            strategies: strategyMap,
+            chosen: Object.keys(strategyMap),
+            report: null,
         };
+    },
+    computed: {
+        chosenStrategies()
+        {
+            return this.chosen.map(name => this.strategies[name]);
+        },
     },
     methods: {
         run() {
             if (this.championship) {
                 this.championship.stop();
             }
+            this.report = null;
             this.championship = new Championship(
-                [
-                    SayAnything,
-                ],
+                this.chosenStrategies,
                 Whos,
                 100
             );
@@ -36,6 +72,7 @@ export default {
                     console.log('onProgress', report);
                 })
                 .onDone(results => {
+                    this.report = results;
                     console.log('onDone', results);
                 })
                 .run();
