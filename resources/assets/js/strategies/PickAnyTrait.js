dad5356e7bc9d20d4702df57480eb2d2bc5d9210
dkuck <dkuck@thatsus.com>
2017-03-28 15:51:21 -0400

    Adjusted to be a nicer example

--- resources/assets/js/strategies/PickAnyTrait.js
+++ resources/assets/js/strategies/PickAnyTrait.js
@@ -15,14 +15,28 @@ const Searcher = require('../Searcher.js');
 class PickAnyTrait
 {
 
-    move(me, other, log){
+    move(me, other, log) {
+        log('Remaining: ' + me.board.remaining.length);
         if (me.board.remaining.length == 1) {
           log('Answering: ' + me.board.remaining[0].name);
           return me.board.remaining[0].name;
+        } else {
+          return this.pickAnyTrait(me, other, log);
         }
-        log('Remaining: ' + me.board.remaining.length);
-        var field = _(me.board.remaining).map((who) => Object.keys(who)).flatten().unique().shuffle().value()[0];
-        var value = _(me.board.remaining).map((who) => who[field]).filter(value => value).shuffle().value()[0];
+    }
+
+    pickAnyTrait(me, other, log) {
+        var field = _(me.board.remaining)
+            .map((who) => Object.keys(who))
+            .flatten()
+            .unique()
+            .shuffle()
+            .value()[0];
+        var value = _(me.board.remaining)
+            .map((who) => who[field])
+            .filter(value => value)
+            .shuffle()
+            .value()[0];
         log('Filtering on ' + field + ' = ' + value);
         return Searcher.where(field, value);
     }
